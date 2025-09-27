f06a03e1edd8bb938db8cb966c533b22e77d6788
Bigmeow99 <wzhen0646@gmail.com>
2025-09-28 00:56:05 +0800

    Site updated: 2025-09-28 00:56:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/27/微信视频号视频下载/","2025/06/05/hello-world/"];function toRandomPost(){
+var posts=["2025/06/27/微信视频号视频下载/","2025/06/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
