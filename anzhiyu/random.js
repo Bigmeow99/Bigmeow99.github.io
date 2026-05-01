f15af14e7d4165bd6c18334fa6745d70d3d9af97
Bigmeow99 <wzhen0646@gmail.com>
2026-05-01 17:22:13 +0800

    Site updated: 2026-05-01 17:22:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/28/微信电脑版视频号视频下载工具/"];function toRandomPost(){
+var posts=["2026/02/28/微信电脑版视频号视频下载工具/","2026/05/01/夸克网盘下载加速和网盘扩容/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
