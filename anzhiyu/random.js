176f196d6bad913e289c50a33fba3407adb850b4
Bigmeow99 <wzhen0646@gmail.com>
2026-07-08 00:06:51 +0800

    Site updated: 2026-07-08 00:06:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/28/微信电脑版视频号视频下载工具/","2026/05/01/夸克网盘下载加速和网盘扩容/"];function toRandomPost(){
+var posts=["2026/02/28/微信电脑版视频号视频下载工具/","2026/05/01/最好用的免费手机录屏工具，手机录音工具，电脑录音工具，超好用的录制软件推荐，主播UP主都在用/","2026/05/01/夸克网盘下载加速和网盘扩容/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
